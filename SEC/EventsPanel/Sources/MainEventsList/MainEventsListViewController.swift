1c57b6a0287dc3a422470a4a07f75539c188fcf9
Wojciech Konury <wojciech.konury@angrynerd.co>
2020-10-24 13:23:06 +0200

    #13 | Top menu

--- SEC/EventsPanel/Sources/MainEventsList/MainEventsListViewController.swift
+++ SEC/EventsPanel/Sources/MainEventsList/MainEventsListViewController.swift
@@ -7,10 +7,13 @@ final class MainEventsListViewController: UIViewController {
     // MARK: - IBOutlets
     
     @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var topMenuView: UIView!
+    @IBOutlet var topMenuTopConstraint: NSLayoutConstraint!
     
     // MARK: - Private Properties
     
     private var dataSource = MainEventsListDataSource()
+    private var range: Range<CGFloat> = (0..<0)
     
     // MARK: - Public Properties
     
@@ -26,7 +29,7 @@ final class MainEventsListViewController: UIViewController {
             MainEventsListRow.event(EventTableViewCellPresentable(title: "Bieg Niepodleglosci 2020",
                                                                   date: "11/11 12:30",
                                                                   city: "Wroclaw",
-                                                                  sport: .running))
+                                                                  sport: .running)),
         ]
         tableView.reloadData()
     }
@@ -35,6 +38,8 @@ final class MainEventsListViewController: UIViewController {
     
     private func setup() {
         navigationController?.isNavigationBarHidden = true
+        
+        range = ((topMenuView.frame.height - 1) * -1)..<0
     }
     
     private func setupTableView() {
@@ -43,12 +48,22 @@ final class MainEventsListViewController: UIViewController {
         tableView.delegate = dataSource
         dataSource.use(mainEventsListDelegate: self)
         tableView.separatorStyle = .none
+        tableView.bounces = false
+        tableView.bouncesZoom = false
     }
 }
 
 extension MainEventsListViewController: MainEventsListViewControllerLogic {}
 
 extension MainEventsListViewController: MainEventsListDelegate {
+    func scrollViewDidScroll(delta: CGFloat) {
+        if delta < 0 {
+            topMenuTopConstraint.constant = min(topMenuTopConstraint.constant - delta, range.upperBound)
+        } else {
+            topMenuTopConstraint.constant = max(range.lowerBound, topMenuTopConstraint.constant - delta)
+        }
+    }
+    
     func didSelectItem(_ mainEventsListItem: MainEventsListRow, atIndex index: Int) {
         // TODO: Handle selection
     }
